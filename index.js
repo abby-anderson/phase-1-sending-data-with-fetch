3db99d57bc6a5078a02ff169490070bc1f963c4e
Abby Anderson <abby.e.anderson@gmail.com>
2021-09-07 19:45:24 -0600

    commit tree 2eb3bf6f6b370d9a8de3b3043d6979818ae0c3a1

--- index.js
+++ index.js
@@ -50,29 +50,34 @@ const submitData = (userName, userEmail) => {
 
     fetch("http://localhost:3000/users", configurationObject)
     .then((response) => response.json())
-    .then((dataObj) => renderUserID(dataObj));
-    // .catch(function(error) {
+    .then((dataObj) => renderUserID(dataObj))
+    .catch(function(errorObj){
+        let errorMessage = document.createElement('li');
+        errorMessage.innerText = errorObj.message;
+        let container = document.querySelector('body');
+        container.appendChild(errorMessage);
+    });
     //     //When writing the callback function for your catch(), expect to receive an object on error with a property, message, containing info about what went wrong. Write code to append this message to the DOM when catch() is called.
-    // })
-
 
     const renderUserID = (dataObj) => {
         console.log(dataObj);
+        console.log(dataObj.id);
+
         //Use a second then() to access this newly converted object. From this object, find the new id and append this value to the DOM.
-
+        
         //this part directly below still needs work! rn just console logging, but will need to figure out what the new id is and how to append to the dom. probs similar to what we did on fetch on demand with forms lab!
-
+        
         //If JSON Server is running and index.html is open in the browser, you can test your code in the console: calling submitData() in the console should cause an id number to appear on the pag
         //there is a user object with an id number in the db.json, so make sure to take note of that object name and key/value pair names, to use in here for sure!
-
-        const userID = document.createElement('li');
+        
+        let userID = document.createElement('li');
         userID.innerText = dataObj.id;
-
-        document.appendChild(userID);
-
+        
+        let container = document.querySelector('body');
+        container.appendChild(userID);
+        
     }
-
-
+    
    // Test 4 - Return the Fetch Chain
     //An amazing feature of fetch() is that if you return it, other functions can tack on their own then() and catch() calls. For this lab, you will need to return the fetch() chain from our submitData function to ensure that the tests run correctly.
 
